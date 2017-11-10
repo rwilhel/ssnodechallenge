a7d442229d0aad359c53632f7542f2f15628e41a
rwilhel <rwilhel@gmail.com>
2017-11-09 17:54:22 -0700

    Installs async for use in project

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 request({
   method: 'GET',
-  url: 'https://api.cryptonator.com/api/ticker/usd-btc',
+  url: 'https://api.cryptonator.com/api/ticker/usd-eth',
   headers: {
     'Content-Type': 'application/json',
   }}, function(error, response, body) {
     let result = JSON.parse(body);
     let data = result.ticker.price;
-    console.log(data);
+    console.log(data*20);
   // }}, function (error, response, body) {
   // console.log('Status:', response.statusCode);
   // console.log('Headers:', JSON.stringify(response.headers));
