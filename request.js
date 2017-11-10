33fe3721e28855a2050cdb2e9fc9282d16afcc20
rwilhel <rwilhel@gmail.com>
2017-11-09 19:54:32 -0700

    Minor adjustments made to styling and file naming

--- request.js
+++ request.js
@@ -1,42 +1,41 @@
-const request = require('request');
-const async = require('async');
+const REQUEST = require('request');
 
 function usdEth() {
-  request({
+  REQUEST({
     method: 'GET',
     url: 'https://api.cryptonator.com/api/ticker/usd-eth',
     headers: {
       'Content-Type': 'application/json',
     }}, function(error, response, body) {
       let result = JSON.parse(body);
-      let data = result.ticker.price;
-      console.log("$20 USD to Ethereum is " + (data*20) + " ETH");
+      dataEth = result.ticker.price;
+      console.log('$20 USD to Ethereum is ' + (dataEth * 20) + ' ETH');
   });
 }
 
 function usdDash() {
-  request({
+  REQUEST({
     method: 'GET',
     url: 'https://api.cryptonator.com/api/ticker/usd-dash',
     headers: {
       'Content-Type': 'application/json',
     }}, function(error, response, body) {
       let result = JSON.parse(body);
-      let data = result.ticker.price;
-      console.log("$20 USD to DASH is " + (data*20) + " DASH");
+      let dataDash = result.ticker.price;
+      console.log('$20 USD to DASH is ' + (dataDash * 20) + ' DASH');
   });
 }
 
 function usdLtc() {
-  request({
+  REQUEST({
     method: 'GET',
     url: 'https://api.cryptonator.com/api/ticker/usd-ltc',
     headers: {
       'Content-Type': 'application/json',
     }}, function(error, response, body) {
       let result = JSON.parse(body);
-      let data = result.ticker.price;
-      console.log("$20 USD to LiteCoin is " + (data*20) + " LTC");
+      let dataLtc = result.ticker.price;
+      console.log('$20 USD to LiteCoin is ' + (dataLtc * 20) + ' LTC');
   });
 }
 
