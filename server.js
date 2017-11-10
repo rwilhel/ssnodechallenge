cbc294a6e036cf4e8c28bfe42bc80523abc894a8
rwilhel <rwilhel@gmail.com>
2017-11-09 18:10:38 -0700

    Creates GET requests for Ethereum, DASH and LiteCoin

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const request = require('request');
+const async = require('async');
 
-request({
-  method: 'GET',
-  url: 'https://api.cryptonator.com/api/ticker/usd-eth',
-  headers: {
-    'Content-Type': 'application/json',
-  }}, function(error, response, body) {
-    let result = JSON.parse(body);
-    let data = result.ticker.price;
-    console.log(data*20);
-  // }}, function (error, response, body) {
-  // console.log('Status:', response.statusCode);
-  // console.log('Headers:', JSON.stringify(response.headers));
-  // console.log('Response:', body[0]);
-});
+function usdEth() {
+  request({
+    method: 'GET',
+    url: 'https://api.cryptonator.com/api/ticker/usd-eth',
+    headers: {
+      'Content-Type': 'application/json',
+    }}, function(error, response, body) {
+      let result = JSON.parse(body);
+      let data = result.ticker.price;
+      console.log("$20 USD to Ethereum is " + (data*20) + " ETH");
+  });
+}
+
+function usdDash() {
+  request({
+    method: 'GET',
+    url: 'https://api.cryptonator.com/api/ticker/usd-dash',
+    headers: {
+      'Content-Type': 'application/json',
+    }}, function(error, response, body) {
+      let result = JSON.parse(body);
+      let data = result.ticker.price;
+      console.log("$20 USD to DASH is " + (data*20) + " DASH");
+  });
+}
+
+function usdLtc() {
+  request({
+    method: 'GET',
+    url: 'https://api.cryptonator.com/api/ticker/usd-ltc',
+    headers: {
+      'Content-Type': 'application/json',
+    }}, function(error, response, body) {
+      let result = JSON.parse(body);
+      let data = result.ticker.price;
+      console.log("$20 USD to LiteCoin is " + (data*20) + " LTC");
+  });
+}
+
+usdEth();
+usdDash();
+usdLtc();
